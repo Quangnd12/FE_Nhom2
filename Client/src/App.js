68b1f35eb49062b99302696843e096afa138016d
ThanhNha3 <nhantpc05188@fpt.edu.vn>
2024-07-19 12:49:35 +0700

    module header, sidebar, footer, homepage

--- Client/src/App.js
+++ Client/src/App.js
@@ -4,15 +4,25 @@ import {
   Routes, Route
 } from "react-router-dom";
 import HomePage from './pages/homepage/HomePage';
+import Header from './components/header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <SideBar />
-      <Routes>
-        <Route path='/' Component={HomePage}></Route>
-      </Routes>
-    </div>
+    <div className="App max-h-dvh bg-black">
+      <div className='flex gap-2' >
+        <div style={{ width: "300px" }}>
+          <SideBar />
+        </div>
+        <div className='flex-1'>
+          <Header />
+          <div className='overflow-y-auto' style={{ background: 'linear-gradient(to bottom, #888888, #000000 95%)', height:"620px" }}>
+            <Routes>
+              <Route path='/' Component={HomePage}></Route>
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </div >
   );
 }
 
