6078061192fc3d9e9a766c7c66fa1b556f11c559
ThanhNha3 <nhantpc05188@fpt.edu.vn>
2024-07-19 12:49:35 +0700

    module header, sidebar, footer, homepage

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,26 +1,62 @@
-import './App.css';
+import "./App.css";
 // import SideBar from './components/sidebar/SideBar.component';
-import {
-  Routes, Route
-} from "react-router-dom";
-import HomePage from './pages/homepage/HomePage';
-import Login from './pages/auth/login';
-import Register from './pages/auth/register';
-import ForgotPass from './pages/auth/forgotPass';
-import SearchPage from './pages/searchpage/SearchPage';
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/homepage/HomePage";
+import Login from "./pages/auth/login";
+import Register from "./pages/auth/register";
+import ForgotPass from "./pages/auth/forgotPass";
+import SearchPage from "./pages/searchpage/SearchPage";
 
 function App() {
   return (
-    <div className="App">
-      {/* <SideBar /> */}
-      <Routes>
-        <Route path='/' Component={HomePage}></Route>
-         <Route path='/login' Component={Login}></Route>
-         <Route path='/register' Component={Register}></Route>
-            <Route path='/forgot' Component={ForgotPass}></Route>
-      </Routes>
-      <SearchPage></SearchPage>
+    <div className="App max-h-dvh bg-black">
+      <div className="flex gap-2">
+        <div style={{ width: "300px" }}>
+          <SideBar />
+        </div>
+        <div className="flex-1">
+          <Header />
+          <div
+            className="overflow-y-auto"
+            style={{ background: "linear-gradient(to bottom, #888888, #000000 95%)", height: "620px" }}
+          >
+            <Routes>
+              <Route
+                path="/"
+                Component={HomePage}
+              ></Route>
+              <Route
+                path="/"
+                Component={HomePage}
+              ></Route>
+              <Route
+                path="/login"
+                Component={Login}
+              ></Route>
+              <Route
+                path="/register"
+                Component={Register}
+              ></Route>
+              <Route
+                path="/forgot"
+                Component={ForgotPass}
+              ></Route>
+            </Routes>
+            <SearchPage></SearchPage>
+          </div>
+        </div>
+      </div>
     </div>
+    // <div className="App">
+    //   {/* <SideBar /> */}
+    //   <Routes>
+    //     <Route path='/' Component={HomePage}></Route>
+    //      <Route path='/login' Component={Login}></Route>
+    //      <Route path='/register' Component={Register}></Route>
+    //         <Route path='/forgot' Component={ForgotPass}></Route>
+    //   </Routes>
+    //   <SearchPage></SearchPage>
+    // </div>
   );
 }
 
