002f7dacbf310ceec3fbf6790e3f00dd96226cf7
QUANG <quangnguyenkz123@gmail.com>
2024-07-19 14:56:46 +0700

    [Build Artist page] + update Search page

--- Client/src/components/directional/Button.jsx
+++ Client/src/components/directional/Button.jsx
@@ -6,7 +6,7 @@ const DirectionalButton = ({ direction }) => {
       className="bg-black text-white font-bold py-2 px-4 rounded-full relative group"
     >
       {direction === 'back' ? '<' : '>'}
-      <span className="absolute top-full mb-2 hidden group-hover:block bg-gray-700 text-white text-xs rounded py-2 px-4">
+      <span className="absolute top-full mb-2 hidden group-hover:block bg-gray-700 text-white text-xs rounded py-2 px-4 whitespace-nowrap">
         {direction === 'back' ? 'Quay lại' : 'Tiếp theo'}
       </span>
     </button>
---
+++ Client/src/data/popularSongs.jsx
@@ -0,0 +1,35 @@
+const popularSongs = [
+    {
+      image: "https://i.scdn.co/image/ab67616d00001e027c17174be9cdaa69349f47d6",
+      name: "Ai Mà Biết Được (feat. tlinh)",
+      details: "1,947,983",
+      duration: "4:00",
+    },
+    {
+      image: "https://i.scdn.co/image/ab67616d00001e027c17174be9cdaa69349f47d6",
+      name: "giá như",
+      details: "4,065,807 ",
+      duration: "3:43",
+    },
+    {
+      image: "https://i.scdn.co/image/ab67616d00001e022922307c16bb852a0849bea0",
+      name: "Anh Đã Quen Với Cô Đơn",
+      details: "10,119,884",
+      duration: "4:28",
+    },
+    {
+      image: "https://i.scdn.co/image/ab67616d00001e0226259b2434afe2ee9808900a",
+      name: "Đi Để Trở Về",
+      details: "9,870,312",
+      duration: "3:22",
+    },
+    {
+      image: "https://i.scdn.co/image/ab67616d00001e02e7f2ddd96a9119666e7f7123",
+      name: "Nước Hoa",
+      details: "435,041",
+      duration: "2:40",
+    },
+  ];
+  
+  export default popularSongs;
+  
