261c1585021818f26c8d4b9b4a2c749986f991a6
ThanhNha3 <nhantpc05188@fpt.edu.vn>
2024-07-31 17:58:07 +0700

    config endpoint + API BASE URL

---
+++ src/endpoint/endpoint.jsx
@@ -0,0 +1,14 @@
+const endpoints = {
+  login: "login",
+  register: "register",
+  users: "users",
+  genres: "genres",
+  artists: "artists",
+  albums: "albums",
+  follows: "follows",
+  lyrics: "lyrics",
+  playlists: "playlists",
+  auth: "auth",
+};
+
+export default endpoints;
